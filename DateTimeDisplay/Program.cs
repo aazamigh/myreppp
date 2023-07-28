75b1d89a1adc25b3506cce4daf69223970c9a2ff
ali <ali@a.a>
2023-07-28 16:41:09 +0330

    - aa03

--- DateTimeDisplay/Program.cs
+++ DateTimeDisplay/Program.cs
@@ -13,6 +13,7 @@ namespace DateTimeDisplay
             DateTime dt=DateTime.Now;
             Console.WriteLine(dt.FormatToString(1));
             Console.WriteLine(dt.FormatToString(7));
+            // no rep 003
         }
     }
 }
